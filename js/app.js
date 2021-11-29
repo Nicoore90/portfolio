433e7747af1e714e43a73cf48e60b5daec97f149
Nicolas Orecchia <nicoaleore@gmail.com>
2021-11-29 12:17:05 -0300

    animaciones grid y navbar

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$("#houseImage").hover(function () {
+    $("#houseText").slideToggle()
+}, function () {
+    $("#houseText").fadeOut()
+})
+
+$("#houseTitle").animate(
+    "margin-left : 0px;")
+
+$("#enciclopediaImage").hover(function () {
+    $("#enciclopediaText").slideToggle()
+ }, function () {
+    $("#enciclopediaText").fadeOut()
+})
